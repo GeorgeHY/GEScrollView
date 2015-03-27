bc60d229a77c165122aeb79d65d230bd9e35021f
GeorgeHan <hanyangkid@163.com>
2015-03-27 10:44:47 +0800

    GEScrollView

---
+++ testADDisplay/GEScrollView.h
@@ -0,0 +1,13 @@
+//
+//  GEScrollView.h
+//  testADDisplay
+//
+//  Created by iwind on 15/3/27.
+//  Copyright (c) 2015年 MUGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEScrollView : UIView
+
+@end
---
+++ testADDisplay/GEScrollView.m
@@ -0,0 +1,21 @@
+//
+//  GEScrollView.m
+//  testADDisplay
+//
+//  Created by iwind on 15/3/27.
+//  Copyright (c) 2015年 MUGH. All rights reserved.
+//
+
+#import "GEScrollView.h"
+
+@implementation GEScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
